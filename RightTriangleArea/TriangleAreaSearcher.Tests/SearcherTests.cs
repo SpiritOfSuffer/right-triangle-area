4f0e21db3f629a3c0b68bcab9e7bd94ff3902963
Deuse <topdeuse@gmail.com>
2017-07-29 01:54:54 +0300

    ini

---
+++ RightTriangleArea/TriangleAreaSearcher.Tests/SearcherTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleAreaSearcherLib;
+
+namespace TriangleAreaSearcher.Tests
+{
+    [TestClass]
+    public class SearcherTests
+    {
+        [TestMethod]
+        public void SearchAreaTest()
+        {
+            Assert.AreEqual(Searcher.SearchArea(3, 4, 5), 6);
+            Assert.AreEqual(Searcher.SearchArea(3.0, 4.0, 5.0), 6.0);
+        }
+
+        [TestMethod]
+        public void DataAreCorrectTest()
+        {
+            Assert.IsTrue(Searcher.DataAreCorrect(3, 4, 5));
+            Assert.IsFalse(Searcher.DataAreCorrect(-3, 4, 5));
+            Assert.IsFalse(Searcher.DataAreCorrect(3, 4, 0));
+            Assert.IsTrue(Searcher.DataAreCorrect(0.1, 0.01, 0.00000001));
+        }
+
+        [TestMethod]
+        public void TriangleIsRightTest()
+        {
+            Assert.IsTrue(Searcher.TriangleIsRight(3, 4, 5));
+            Assert.IsFalse(Searcher.TriangleIsRight(3, 9, 9));
+            Assert.IsFalse(Searcher.TriangleIsRight(-3, 0, 2));
+            
+        }
+    }
+}
---
+++ RightTriangleArea/TriangleAreaSearcherLib/Searcher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleAreaSearcherLib
+{
+    public class Searcher
+    {
+        //Метод находит площадь треугольника по формуле Герона. Общий вид формулы: S=sqrt(p(p-a)(p-b)(p-c)), где p - полупериметр
+        public static double SearchArea(double FirstSide, double SecondSide, double ThirdSide)
+        {
+                double Semiperimeter = (FirstSide + SecondSide + ThirdSide) / 2; //Находим полупериметр
+
+                return Math.Sqrt(Semiperimeter * (Semiperimeter - FirstSide) * (Semiperimeter - SecondSide) * (Semiperimeter - ThirdSide));
+        }
+
+        //Метод определяет, корректны ли введенные данные
+        public static bool DataAreCorrect(double FirstSide, double SecondSide, double ThirdSide)
+        {
+            return FirstSide > 0.0 && SecondSide > 0.0 && ThirdSide > 0.0;
+        }
+
+        //Метод определяет, является ли треугольник прямоугольным
+        public static bool TriangleIsRight(double FirstSide, double SecondSide, double ThirdSide)
+        {
+            if (DataAreCorrect(FirstSide, SecondSide, ThirdSide))
+            {
+                bool IsRight = false;
+                double[] data = new double[3] { FirstSide, SecondSide, ThirdSide }; //Массив из 3-х сторон треугольника
+                Array.Sort(data); //Сортируем массив
+                IsRight = (data[0] * data[0]) + (data[1] * data[1]) == data[2] * data[2]; //Определяем по теореме Пифагора, прямоугольный ли наш треугольник 
+
+                return IsRight;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
